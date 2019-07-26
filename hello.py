c48d2f1361ff772052540c8f0abaf92575a9b56b
gwc <pinkpassion4life999@gmail.com>
2019-07-26 13:54:18 -0700

    add hello.py to repository

---
+++ hello.py
@@ -0,0 +1,65 @@
+# print("Hello, World!")
+# answer = input("Who inspires you?")
+# print(answer, "inspires you!")
+# while True:
+#     print("forever looping!")
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+# for i in range(1, 5, 2)
+#     print(i)
+# i = -1
+# while True:
+    # i +=1
+#     if (i > 20):
+#         break
+#     # i is odd
+#     if(i % 2 != 0):
+#         continue
+#
+#     print(i)
+
+# print ("It's 7:15, my alarm is ringing!")
+# answer =input("snooze or wake up?")
+# # user_input = input()
+# if answer == "snooze":
+#     print("...")
+#     print("Yay! 10 more minutes!")
+# elif answer == "wake up":
+#     print("...")
+#     print("Boo what a party pooper >:O")
+# print ("My friend is asking me to hang out.")
+# answer2 =input("accept or decline?")
+# if answer2 == "accept":
+#     print("...")
+#     print("Okay I'll meet you outside...")
+#     print("Little do you know that my friends are wanted criminals! MUAHAHAHA")
+#     print("Should I participate in the CTO-napping of Can Mawz?")
+#     answer3 =input("yes or no?")
+#     if answer3 == "yes":
+#         print("...")
+#         print("Alright! Let's do it!")
+#         print("5 days later...")
+#         print("MISSION FAILED!")
+#         print("See yalls in prison *peace sign*")
+#         print("THE END")
+#     elif answer3 == "no":
+#         print("...")
+#         print("Aw you're no fun")
+#         print("THE END")
+# elif answer2 == "decline":
+#     print("...")
+#     print("Yay guess I'll be a loner :)")
+#     print("I'm bored. Should I CTO-stalk Can Mawz on instagram?")
+#     answer4 =input("yeah or nah?")
+#     if answer4 == "yeah":
+#         print("...")
+#         print ("*Stalks for 5 hours*")
+#         print("THIS IS THE FBI. REFRAIN FROM PURSUING CAN MAWZ OR EXPECT CONSEQUENCES.")
+#         print("And I oop-")
+#         print("THE END")
+#     elif answer4 == "nah":
+#         print("...")
+#         print("Fine. Guess I'm taking a nap.")
+#         print("THE END")
